b04dc7840c5fc8c42d90d4a493f3c3fcd6ada92e
mauricio <mauricio.vera.r7@gmail.com>
2022-05-06 11:01:57 -0400

    pwa

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to PWA <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
